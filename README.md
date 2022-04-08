85b6626172f777685fd54b7cedcbfd80f7c2b05f
mat.harvey@rise-x.io <mat.harvey@rise-x.io>
2022-04-08 10:45:39 +0800

    for evan x

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# video_chat_OneToMany
+
+## Usage
+* Run npm install.
+* Add your firebase config details to Server/firebase.js. 
+* Run "npm run start" to start the app. 
+ 
---
+++ src/server/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAWMBLk19v4c_B1jl69OtlJwaqmSGB", // Add API Key
+  databaseURL:"https://one-to-many-videochat-default-rtdb.asia-southeast1.firebasedatabase.app/" // Add databaseURL
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase;
+
+var firepadRef = firebase.database().ref();
+
+export const userName = prompt("What's your name?");
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
+
+if (roomId) {
+  firepadRef = firepadRef.child(roomId);
+} else {
+  firepadRef = firepadRef.push();
+  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
+}
+
+export default firepadRef;
